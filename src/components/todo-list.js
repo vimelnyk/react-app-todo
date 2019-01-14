7fe218577d33be96f19e75f6ed5260fd5ea1d4f6
Vira Melnyk <vira.melnyk@vordik.com>
2019-01-14 10:00:34 +0100

    Import CSS

--- src/components/todo-list.js
+++ src/components/todo-list.js
@@ -1,14 +1,25 @@
 import React from 'react';
 
-import TodoListItem from './todo-list-item'
+import TodoListItem from './todo-list-item';
+import './todo-list.css';
 
-const TodoList = () => {
-	return (
-		<ul>
-			<li><TodoListItem label="Drink Cofee" /></li>
-			<li><TodoListItem label="Create React App" important /></li>
-		</ul>
-	)
-}
+const TodoList = ({ todos }) => {
+
+  const elements = todos.map((item) => {
+    const { id, ...itemProps } = item;
+
+    return (
+      <li key={id} className="list-group-item">
+        <TodoListItem {...itemProps } />
+      </li>
+    );
+  });
+
+  return (
+    <ul className="list-group todo-list">
+      { elements }
+    </ul>
+  );
+};
 
 export default TodoList;
--- src/index.js
+++ src/index.js
@@ -6,11 +6,18 @@ import SearchPanel from './components/search-panel'
 import TodoList from './components/todo-list'
 
 const App = () => {
+
+	const todoData = [
+		{label: 'Drink Cofee', important: false, id: 1},
+		{label: 'Make Awsome App', important: true, id: 2},
+		{label: 'Have a lunch', important: false, id: 3}
+	]
+
 	return(
-		<div>
-			<AppHeader />
+		<div className="container">
+			<AppHeader toDo={1} done={3} />			
 			<SearchPanel />
-			<TodoList />
+			<TodoList todos={todoData} />
 		</div>
 		);
 }
